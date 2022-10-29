bcc412eba119a9607f3872a4cd90e3c9779da8c8
Caesar <ing.cesar.canto@gmail.com>
2022-10-29 08:44:48 -0500

    Attack Signal added to Enemy Attack
    Also a small bug where the collider of the enemy's attack remained active after the attack if did not hit the player was delt.
    
    The problem was the animation cut before calling he last animation event where it disabled the collider.

--- Bug_Samurai/Assets/AttackCollider.cs
+++ Bug_Samurai/Assets/AttackCollider.cs
@@ -6,10 +6,12 @@ public class AttackCollider : MonoBehaviour
 {
 
     public void EnableCollider(){
+        print("Enable Collider");
         GetComponent<BoxCollider2D>().enabled=true;
     }
 
     public void DisableCollider(){
+        print("Disable Collider");
         GetComponent<BoxCollider2D>().enabled = false;
     }
     private void OnTriggerStay2D(Collider2D other) {
