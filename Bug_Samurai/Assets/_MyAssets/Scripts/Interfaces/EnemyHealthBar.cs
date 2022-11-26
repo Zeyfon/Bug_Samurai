d92a425256caa797e0d29a1dfca9b1bc8840a6e4
Caesar <ing.cesar.canto@gmail.com>
2022-11-25 19:05:01 -0600

    Enemy Template and Manage were updated
    Several bugs on enemies were fixed
    Scripts were moved

--- Bug_Samurai/Assets/_MyAssets/Scripts/Interfaces/EnemyHealthBar.cs
+++ Bug_Samurai/Assets/_MyAssets/Scripts/Interfaces/EnemyHealthBar.cs
@@ -7,20 +7,15 @@ public class EnemyHealthBar : MonoBehaviour
 
     EnemyHealth health;
     [SerializeField] Transform barTransform;
+    [SerializeField] float maxHealthValueForBar;
 
-    float healthValue;
-    float maxHealth;
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         health = GetComponentInParent<EnemyHealth>();
-        maxHealth = health.GetHealth();
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthValue = health.GetHealth();
-        barTransform.localScale = new Vector3((healthValue/maxHealth), barTransform.localScale.y,barTransform.localScale.z);
+        barTransform.localScale = new Vector3((health.GetHealth()/maxHealthValueForBar), barTransform.localScale.y,barTransform.localScale.z);
     }
 }
